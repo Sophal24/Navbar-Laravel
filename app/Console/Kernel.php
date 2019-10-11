b8478019ac9ee8cb8daaf3203a7f74c48dd191b7
Sophal24 <sophalisco23@gmail.com>
2019-10-12 00:44:56 +0700

    update cronjob

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('save:weather')
-                ->dailyAt('1:00')
+                ->dailyAt('1:00');
                  // ->everyMinute();
                 // ->timezone('Asia/Phnom_Penh');
 
