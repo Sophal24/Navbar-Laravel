c363a78a92220a7e40aee936fd3c8da4e56f0325
Sophal24 <sophalisco23@gmail.com>
2019-10-19 14:41:18 +0700

    notification

--- app/Http/Controllers/Controller/subscriberController.php
+++ app/Http/Controllers/Controller/subscriberController.php
@@ -12,55 +12,55 @@ class subscriberController extends Controller
 {
     //save subscriber when they subscribe the service
 
-  //   public function savesub(Request $request){
-  //   	$data = new subscriberModel;
-
-  //   	$data->subscriberId = $request->input('subscriberId');
-  //   	$data->status = $request->input('status');
-  //   	$data->frequency = $request->input('frequency');
-
-  //   	$data->save();
-		// // echo "OK";
-  //   }
-
     public function savesub(Request $request){
     	$data = new subscriberModel;
 
-    	// $data->save();
-    	$subid = $request->input('subscriberId');
+    	$data->subscriberId = $request->input('subscriberId');
+    	$data->status = $request->input('status');
+    	$data->frequency = $request->input('frequency');
 
-    	$user_favorites = DB::table('subscribers')
-		    ->where('subscriberId', '=', $subid)
-		    ->first();
-
-  		if (is_null($user_favorites)) {
-  		    // It does not exist - add to favorites button will show
-  		    // echo "no";
-
-  		    $data->subscriberId = $request->input('subscriberId');
-      		$data->status = $request->input('status');
-      		$data->frequency = $request->input('frequency');
-
-      		$data->save();
-      		// echo "Brand new user was Saved";
-
-  		} else {
-  		    // It exists - remove from favorites button will show
-  		    // echo "yes";
-  		    DB::table('subscribers')
-  		    ->where('subscriberId', '=', $subid)
-  		  	->delete();
-  		    // echo "Done Delete and ";
-
-  		    $data->subscriberId = $request->input('subscriberId');
-      		$data->status = $request->input('status');
-      		$data->frequency = $request->input('frequency');
-
-      		$data->save();
-      		// echo "Saved";
-
-  		}
+    	$data->save();
+		// echo "OK";
     }
+
+    // public function savesub(Request $request){
+    // 	$data = new subscriberModel;
+
+    // 	// $data->save();
+    // 	$subid = $request->input('subscriberId');
+
+    // 	$user_favorites = DB::table('subscribers')
+		  //   ->where('subscriberId', '=', $subid)
+		  //   ->first();
+
+  		// if (is_null($user_favorites)) {
+  		//     // It does not exist - add to favorites button will show
+  		//     // echo "no";
+
+  		//     $data->subscriberId = $request->input('subscriberId');
+    //   		$data->status = $request->input('status');
+    //   		$data->frequency = $request->input('frequency');
+
+    //   		$data->save();
+    //   		// echo "Brand new user was Saved";
+
+  		// } else {
+  		//     // It exists - remove from favorites button will show
+  		//     // echo "yes";
+  		//     DB::table('subscribers')
+  		//     ->where('subscriberId', '=', $subid)
+  		//   	->delete();
+  		//     // echo "Done Delete and ";
+
+  		//     $data->subscriberId = $request->input('subscriberId');
+    //   		$data->status = $request->input('status');
+    //   		$data->frequency = $request->input('frequency');
+
+    //   		$data->save();
+    //   		// echo "Saved";
+
+  		// }
+    // }
 
 
 
