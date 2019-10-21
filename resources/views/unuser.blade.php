d583cdf87052f04eafe4bd3138d7aaf5b5027e3e
Sophal24 <sophalisco23@gmail.com>
2019-10-22 00:21:10 +0700

    update savesub and frequency text

--- resources/views/unuser.blade.php
+++ resources/views/unuser.blade.php
@@ -43,7 +43,7 @@
                         <td>{{ Str::limit($row->subscriberId,15) }}</td>
                         <!-- <td>{{ $row->subscriberId }}</td> -->
                         <td>{{ $row->status }}</td>
-                        <td>{{ $row->frequency }}</td>
+                        <td>Monthly</td>
                         <td><a onclick="return confirm('Are you sure?')" href="/delete/{{ $row->id }}"><button type="button" class="btn btn-danger"><i class="fas fa-times"></i> Delete</button></a></td>
                       </tr>
                       @endforeach
