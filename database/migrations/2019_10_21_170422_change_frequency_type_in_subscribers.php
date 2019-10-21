c9c0d421ac46aa46c3d020939f0bbca00dc6ee12
Sophal24 <sophalisco23@gmail.com>
2019-10-22 00:08:01 +0700

    update savesub

--- database/migrations/2019_10_21_170422_change_frequency_type_in_subscribers.php
+++ database/migrations/2019_10_21_170422_change_frequency_type_in_subscribers.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSubscribersTable extends Migration
+class ChangeFrequencyTypeInSubscribers extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateSubscribersTable extends Migration
      */
     public function up()
     {
-        Schema::create('subscribers', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('subscriberId');
-            $table->string('status');
+        Schema::table('subscribers', function (Blueprint $table) {
+            //
             $table->string('frequency')->nullable()->change();
-            $table->timestamps();
         });
     }
 
@@ -29,6 +26,8 @@ class CreateSubscribersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('subscribers');
+        Schema::table('subscribers', function (Blueprint $table) {
+            //
+        });
     }
 }
