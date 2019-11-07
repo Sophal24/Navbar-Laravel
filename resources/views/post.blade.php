0e3055ce0c2fdb06413090d7fdbbb8fe0e8326bc
Sophal24 <sophalisco23@gmail.com>
2019-11-07 11:21:43 +0700

    update textarea

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -20,7 +20,7 @@
 
                         <h2 style="font-size: 35px;">Post</h2>
                         <div class="form-group">
-                            <textarea class="form-control" maxlength="250" name="text" id="exampleFormControlTextarea1" rows="3" style="height: 180px;" placeholder="Text to send news to client, must be under 250 characters" required></textarea>
+                            <textarea class="form-control" maxlength="250" name="text" id="exampleFormControlTextarea1" rows="3" style="height: 160px;" placeholder="Text to send news to client, must be under 250 characters" required></textarea>
                         </div>
                         <button type="submit" class="btn btn-secondary btn-lg btn-block btn-info" style="margin-top: 33px;">Send</button>
 
